cb0e2f9fe8c5d3fb15112860f5f9dadbc214fff1
Bodhisha Thomas <bodhisha@gmail.com>
2020-05-26 01:07:19 +0530

    layout with rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+                height: double.infinity,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    margin: EdgeInsets.only(left: 55, right: 55),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    margin: EdgeInsets.only(left: 55, right: 55),
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: double.infinity,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
